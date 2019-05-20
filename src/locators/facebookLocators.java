b562e46d6e6d9390cc5f405921ea52985d0945dc
ashfaqgithub <huque.md.ashfaqul@gmail.com>
2019-05-19 21:17:46 -0400

    111

---
+++ src/locators/facebookLocators.java
@@ -0,0 +1,14 @@
+package locators;
+
+public class facebookLocators {
+
+
+	public	String email = "//*[@id='u_0_h']";
+	public	String pass = "//input[@type='password']";
+
+	public String fnLoc = "//input[@name='firstname']";
+	public String lnLoc = "//input[@name='lastname']";
+	
+	public String signUpText = "//*[@id='content']/div/div/div/div/div[2]/h2";
+
+}
